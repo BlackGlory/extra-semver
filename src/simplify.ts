9a0fb29e5bb799375d58f7161517980405f8ffdb
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-14 05:17:02 +0000

    refactor: improve readability

--- src/simplify.ts
+++ src/simplify.ts
@@ -4,9 +4,8 @@ import { assert } from '@blackglory/errors'
 export function simplify(version: string): string {
   const semver = parse(version)
   assert(semver, 'version must be valid semver')
-  if (semver.major === 0) {
-    return `${semver.major}.${semver.minor}`
-  } else {
-    return `${semver.major}`
-  }
+
+  return semver.major === 0
+       ? `${semver.major}.${semver.minor}`
+       : `${semver.major}`
 }
